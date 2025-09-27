5c93f0b01d470f277784125364ab9f16fe5475d1
xboxser <xboxser74@yandex.ru>
2025-09-27 12:17:44 +0500

    fix autotest validator

--- internal/user/validator/api.go
+++ internal/user/validator/api.go
@@ -11,6 +11,9 @@ import (
 // Проверяем обязательные поля
 // Скрываем ошибки формата "Key: 'ApiUser.Login' Error:Field validation for 'Login' failed on the 'required' tag"
 func ValidateModelUserAPI(user model.APIUser) error {
+	if projectValidator.Validate == nil {
+		projectValidator.Init()
+	}
 	err := projectValidator.Validate.Struct(user)
 	if err != nil {
 		if validationErrors, ok := err.(validator.ValidationErrors); ok {
